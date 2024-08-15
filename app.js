cd5301ecf79240a3307b17676d6f8ca5003212fb
Aram Hekimian <guerrerohek121@gmail.com>
2024-08-15 11:20:33 -0600

    Implemented mobile image slider functionality

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const darkOverlay = document.querySelector(".overlay");
 const desktopImage = document.querySelector(".desktop__image");
 const lightbox = document.querySelector(".lightbox");
 const lightboxClose = document.querySelector(".lightbox__close");
+const mobileImageBtns = document.querySelectorAll(".mobile__image--btn");
+let currentImage = "";
+currentImage = getImage();
 
 // Show mobile nav
 mobileNavOpen.addEventListener("click", () => {
@@ -29,3 +32,31 @@ desktopImage.addEventListener("click", () => {
 lightboxClose.addEventListener("click", () => {
   lightbox.classList.add("hidden");
 });
+
+// Mobile image slider
+mobileImageBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    if (e.target.classList[1] === "mobilePrev") {
+      currentImage = getImage("prev");
+      e.target.parentNode.style.background = currentImage;
+    } else {
+      currentImage = getImage("next");
+      e.target.parentNode.style.background = currentImage;
+    }
+  });
+});
+
+// Get prev/next image
+function getImage(direction) {
+  if (!currentImage) {
+    return "url('./images/image-product-1.jpg')  top / cover no-repeat";
+  } else if (direction === "next") {
+    let num = +currentImage.split("-")[2].split(".")[0] + 1;
+    let next = num === 5 ? 1 : num;
+    return `url("./images/image-product-${next}.jpg") top / cover no-repeat`;
+  } else if (direction === "prev") {
+    let num = +currentImage.split("-")[2].split(".")[0] - 1;
+    let prev = num === 0 ? 4 : num;
+    return `url("./images/image-product-${prev}.jpg") top / cover no-repeat`;
+  }
+}
