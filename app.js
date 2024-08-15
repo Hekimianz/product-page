c2a8277590eb631659792db7aee9d768830a60b9
Aram Hekimian <guerrerohek121@gmail.com>
2024-08-15 10:46:20 -0600

    Implemented lightbox functionality

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const mobileNavOpen = document.querySelector(".menu__icon--open");
 const mobileNavClose = document.querySelector(".menu__icon--close");
 const mobileNav = document.querySelector(".mobile__nav");
 const darkOverlay = document.querySelector(".overlay");
+const desktopImage = document.querySelector(".desktop__image");
+const lightbox = document.querySelector(".lightbox");
+const lightboxClose = document.querySelector(".lightbox__close");
 
 // Show mobile nav
 mobileNavOpen.addEventListener("click", () => {
@@ -15,4 +18,14 @@ mobileNavClose.addEventListener("click", () => {
   document.body.style.overflow = "scroll";
   mobileNav.classList.add("hidden");
   darkOverlay.classList.add("hidden");
+});
+
+// Open lightbox
+desktopImage.addEventListener("click", () => {
+  lightbox.classList.remove("hidden");
+});
+
+// Close lightbox
+lightboxClose.addEventListener("click", () => {
+  lightbox.classList.add("hidden");
 });
